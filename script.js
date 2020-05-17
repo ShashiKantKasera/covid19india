6afb30150ebd447d730c6aad70f7a2734c3156e1
SHASHI KANT KASERA <46868375+ShashiKantKasera@users.noreply.github.com>
2020-05-17 14:39:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
          var myChart = document.getElementById("myChart").getContext('2d')
         
          var chart = new Chart(myChart,{
-             type:'bar',
+             type:'line',
              data:{
                  labels:state,
                  datasets:[
